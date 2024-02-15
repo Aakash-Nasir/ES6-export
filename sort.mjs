2f5f49a3b942f144e99344d670bbb4f74d5c78cf
Aakash-Nasir <aakashnasir9@gmail.com>
2024-02-15 08:15:26 +0530

    Added changes

---
+++ sort.mjs
@@ -0,0 +1,8 @@
+import searchingAlgo from './sorting.mjs';
+
+import * as sorting from './sorting.mjs';
+
+console.log(searchingAlgo.linearSearch([1,3,45,6,73,23,45],6))
+let arr = [4,5,6,1,27,7];
+sorting.bubblesort(arr);
+console.log(arr);
